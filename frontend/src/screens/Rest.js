ae363e3933b0be1cfe94c3bcb4fd0d2501b545c6
noorbabar <Nbabar233@gmail.com>
2024-04-11 14:53:51 +1000

    setup

--- frontend/src/screens/Rest.js
+++ frontend/src/screens/Rest.js
@@ -1,10 +1,39 @@
 function Rest() {
     return (
+
       <div>
-        <h1 className="text-3xl font-bold underline">
-            Rest
+        <h1 className="text-3xl font-bold">
+          <span className="text-zinc-200">Achieve</span><span className="text-[#2ADCB1]">Mint</span>
+          {/* <img src=""></img> */}
         </h1>
-      </div>
+
+        <h1 className="text-3xl font-bold underline"> Rest </h1>
+
+        <h2 className="text-2xl font-bold mt-8">TAKE A BREAK & DE-STRESS</h2>
+        {/* Take a Break & De-Stress */}
+        
+        <div className="mt-8">
+          <h2 className="text-2xl font-bold">CATEGORY 1</h2>
+          {/* Category 1 - Hungry? */}
+          <p className="text-lg">Positive Affirmation</p>
+          {/* Image 1 - eg yallah eats */}
+        </div>
+
+        <div className="mt-8">
+          <h2 className="text-2xl font-bold">CATEGORY 2</h2>
+          {/* Category 2 - Need Fresh Air? */}
+          {/* Image 1 - eg meditation  */}
+          <p className="text-lg">Positive Affirmation</p>
+        </div>
+        
+        <div className="mt-8">
+          <h2 className="text-2xl font-bold">CATEGORY 3</h2>
+          {/* Category 3 - Tired ? */}
+          {/* Image 1- eg netflix */}
+          <p className="text-lg">Positive Affirmation</p>
+         </div>
+        </div>
+
     );
 }
   
