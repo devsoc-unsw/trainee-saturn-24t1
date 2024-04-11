960fab0954dc9a9565163772008bb6911f096754
noorbabar <Nbabar233@gmail.com>
2024-04-11 15:20:41 +1000

    layout

--- frontend/src/screens/Rest.js
+++ frontend/src/screens/Rest.js
@@ -1,28 +1,35 @@
 function Rest() {
     return (
 
-      <div>
-        <h1 className="text-3xl font-bold">
+      <div className="h-screen p-2 bg-[#302E28]">
+      <h1 className="text-3xl font-bold">
           <span className="text-zinc-200">Achieve</span><span className="text-[#2ADCB1]">Mint</span>
           {/* <img src=""></img> */}
         </h1>
 
-        <h1 className="text-3xl font-bold underline"> Rest Page</h1>
+      <div className="flex flex-col items-center">
 
-        <h2 className="text-2xl font-bold mt-8">TAKE A BREAK & DE-STRESS</h2>
-        {/* Take a Break & De-Stress */}
+        <div className="text-2xl font-bold mt-8 self-center bg-blue-100 px-4 py-2 rounded">
+          TAKE A BREAK & DE-STRESS
+        </div>
         
-        <div className="mt-8">
+        <div className="mt-8 ">
           <h2 className="text-2xl font-bold">CATEGORY 1</h2>
           {/* Category 1 - Hungry? */}
           <p className="text-lg">Positive Affirmation</p>
           {/* Image 1 - eg yallah eats */}
+          {/* Image 2 */}
+          {/* Image 3 */}
+          {/* Image 4 */}
         </div>
 
         <div className="mt-8">
           <h2 className="text-2xl font-bold">CATEGORY 2</h2>
           {/* Category 2 - Need Fresh Air? */}
           {/* Image 1 - eg meditation  */}
+          {/* Image 2 */}
+          {/* Image 3 */}
+          {/* Image 4 */}
           <p className="text-lg">Positive Affirmation</p>
         </div>
         
@@ -30,10 +37,13 @@ function Rest() {
           <h2 className="text-2xl font-bold">CATEGORY 3</h2>
           {/* Category 3 - Tired ? */}
           {/* Image 1- eg netflix */}
+          {/* Image 2 */}
+          {/* Image 3 */}
+          {/* Image 4 */}
           <p className="text-lg">Positive Affirmation</p>
          </div>
         </div>
-
+  </div>  
     );
 }
   
