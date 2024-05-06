ba16b17f24af3254b74d69992344bdc9b3284178
Giselle <cryponiess@gmail.com>
2024-05-06 15:19:18 +1000

    removed useEffect

--- frontend/src/screens/components-hana/ListOfTabs.js
+++ frontend/src/screens/components-hana/ListOfTabs.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import { useState, useRef } from "react";
 import { v4 as uuidv4 } from 'uuid';
 import Task from "./Task.js";
 import PopUpAlert from "../PopUpAlert.js";
