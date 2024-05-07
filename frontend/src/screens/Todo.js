d30b86d4236c75fd600e68eed0fe564778435bb4
Giselle <cryponiess@gmail.com>
2024-05-07 12:15:51 +1000

    updated so that Todo can pass props from ListOfTabs to ProgressBar

--- frontend/src/screens/Todo.js
+++ frontend/src/screens/Todo.js
@@ -2,8 +2,17 @@ import GoalsNotes from './GoalsNotes';
 import PopUpQuotes from './PopUpQuote';
 import ListOfTabs from './components-hana/ListOfTabs'
 import ProgressBar from './components-hana/ProgressBar';
+import { useState } from "react";
 
 function Todo() {
+  const [tabs, setTabs] = useState([]);
+  const [currentTab, setCurrentTab] = useState("");
+
+  const handleData = (data) => {
+    setTabs(data.data1);
+    setCurrentTab(data.data2);
+  };
+
   return (
     <div className="h-screen p-2 bg-[#302E28]">
       <div id="header" className="grid grid-cols-3 justify-around m-2 py-4
@@ -29,15 +38,14 @@ function Todo() {
 
         <div id="to-do-list" className="min-w-96 m-4 basis-1/3">
 
-          <ListOfTabs />
+          <ListOfTabs onData={handleData} />
 
         </div>
 
         <div id="features" className="mx-4 basis-1/3">
-          {/* {bg-[#877070]} */}
           <div id="progress-bar" className="m-1 p-2 ">
             {/* shows how many tasks you've completed so far */}
-            <ProgressBar />
+            <ProgressBar tabs={tabs} currentTab={currentTab} />
           </div>
 
           <div id="alerts" className="m-2 py-3 px-5 bg-[#D4DCFF] rounded-lg">
