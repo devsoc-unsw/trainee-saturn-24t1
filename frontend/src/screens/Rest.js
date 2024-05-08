254d099ba13c9abacd857f436fb53c16934a274a
noorbabar <Nbabar233@gmail.com>
2024-05-08 16:09:52 +1000

    images

--- frontend/src/screens/Rest.js
+++ frontend/src/screens/Rest.js
@@ -1,6 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+ // Import images for categories //
+import image1 from './images/image1.png';
+import image2 from './images/image2.jpeg'; 
+import image3 from './images/image3.jpeg';
+import image4 from './images/image4.jpeg';
+import image5 from './images/image5.jpeg';
+import image6 from './images/image6.jpeg';
+import image7 from './images/image7.jpeg';
+import image8 from './images/image8.jpeg';
+import image9 from './images/image9.jpeg';
+import image10 from './images/image10.jpeg';
+import image11 from './images/image11.webp';
+import image12 from './images/image12.jpeg';
+import image13 from './images/image13.jpeg';
+import image14 from './images/image14.jpeg';
+import image17 from './images/image17.jpeg';
+import image19 from './images/image19.jpeg';
+import image18 from './images/image18.jpeg';
+import image20 from './images/image20.jpeg';
+import image21 from './images/image21.jpeg';
+import image22 from './images/image22.webp';
+import image23 from './images/image23.jpeg';
+import image24 from './images/image24.webp';
+import image25 from './images/image25.jpeg';
+import image26 from './images/image26.jpeg';
+import image27 from './images/image27.jpeg';
+import image28 from './images/image28.jpeg';
+import image29 from './images/image29.jpeg';
+
 /* Timer for rest page: no stop or start button, it just tracks how long you've been on the page for */
 function Timer() {
     const [time, setTime] = useState(0);
@@ -111,64 +140,95 @@ function Rest() {
                         <h2 className="text-2xl font-bold mt-8 self-center rounded text-white transition duration-300 transform hover:scale-105">
                             FOOD SUGGESTIONS
                         </h2>
+                    <div className="image-container">
                         <div className="image-row">
-                            <div className="image-box"></div> {/* Image Box 1 */}
-                            <div className="image-box"></div> {/* Image Box 2 */}
-                            <div className="image-box"></div> {/* Image Box 3 */}
-                            <div className="image-box"></div> {/* Image Box 4 */}
-                            <div className="image-box"></div> {/* Image Box 5 */}
+                        <div className="image-box" style={{ backgroundImage: `url(${image2})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div> {/* Image Box 1 */}
+                        <div className="image-box" style={{ backgroundImage: `url(${image3})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div> {/* Image Box 2 */}
+                        <div className="image-box" style={{ backgroundImage: `url(${image4})`, backgroundSize: 'cover', backgroundPosition: 'center top' }}></div> {/* Image Box 3 */}
+                        <div className="image-box" style={{ backgroundImage: `url(${image5})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div> {/* Image Box 4 */}
+                        <div className="image-box" style={{ backgroundImage: `url(${image6})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
+                        <div className="image-box" style={{ backgroundImage: `url(${image1})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
+                        <div className="image-box" style={{ backgroundImage: `url(${image25})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
+                        <div className="image-box" style={{ backgroundImage: `url(${image29})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
+
                         </div>
                     </div>
+                </div>
 
                     <div className="mt-8 w-full flex flex-col items-center">
                         <h2 className="text-2xl font-bold mt-8 self-center rounded text-white transition duration-300 transform hover:scale-105">
                             OUTDOOR ACTIVITIES
                         </h2>
+                    <div className="image-container">
                         <div className="image-row">
-                            <div className="image-box"></div> {/* Image Box 1 */}
-                            <div className="image-box"></div> {/* Image Box 2 */}
-                            <div className="image-box"></div> {/* Image Box 3 */}
-                            <div className="image-box"></div> {/* Image Box 4 */}
-                            <div className="image-box"></div> {/* Image Box 5 */}
+                        <div className="image-box" style={{ backgroundImage: `url(${image7})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div> {/* Image Box 1 */}
+                        <div className="image-box" style={{ backgroundImage: `url(${image8})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div> {/* Image Box 1 */}
+                        <div className="image-box" style={{ backgroundImage: `url(${image28})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div> {/* Image Box 1 */}
+                        <div className="image-box" style={{ backgroundImage: `url(${image19})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div> {/* Image Box 1 */}
+                        <div className="image-box" style={{ backgroundImage: `url(${image10})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div> {/* Image Box 1 */}
+                        <div className="image-box" style={{ backgroundImage: `url(${image11})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div> {/* Image Box 1 */}
+                        <div className="image-box" style={{ backgroundImage: `url(${image18})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div> {/* Image Box 1 */}
+                        <div className="image-box" style={{ backgroundImage: `url(${image20})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div> {/* Image Box 1 */}
+                        <div className="image-box" style={{ backgroundImage: `url(${image21})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div> {/* Image Box 1 */}
+                        <div className="image-box" style={{ backgroundImage: `url(${image22})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div> {/* Image Box 1 */}
+                        <div className="image-box" style={{ backgroundImage: `url(${image24})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div> {/* Image Box 1 */}
+                        <div className="image-box" style={{ backgroundImage: `url(${image27})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div> {/* Image Box 1 */}
+
                         </div>
                     </div>
+                 </div>
 
                     <div className="mt-8 w-full flex flex-col items-center">
                         <h2 className="text-2xl font-bold mt-8 self-center rounded text-white transition duration-300 transform hover:scale-105">
                             COMFORT ACTIVITIES
                         </h2>
+                    <div className="image-container">
                         <div className="image-row">
-                            <div className="image-box"></div> {/* Image Box 1 */}
-                            <div className="image-box"></div> {/* Image Box 2 */}
-                            <div className="image-box"></div> {/* Image Box 3 */}
-                            <div className="image-box"></div> {/* Image Box 4 */}
-                            <div className="image-box"></div> {/* Image Box 5 */}
+                            <div className="image-box" style={{ backgroundImage: `url(${image12})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div> {/* Image Box 1 */}
+                            <div className="image-box" style={{ backgroundImage: `url(${image14})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div> {/* Image Box 1 */}
+                            <div className="image-box" style={{ backgroundImage: `url(${image13})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div> {/* Image Box 1 */}
+                            <div className="image-box" style={{ backgroundImage: `url(${image17})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div> {/* Image Box 1 */}
+                            <div className="image-box" style={{ backgroundImage: `url(${image9})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div> {/* Image Box 1 */}
+                            <div className="image-box" style={{ backgroundImage: `url(${image23})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div> {/* Image Box 1 */}
+                            <div className="image-box" style={{ backgroundImage: `url(${image26})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div> {/* Image Box 1 */}
+
                         </div>
+                    </div>
                     </div>
                 </div>
             </div>
 
-            {/* styles for image boxes that will b replaced by the images  */}
-            <style>
-                {`
-                .image-row {
-                    display: flex;
-                    flex-wrap: wrap;
-                }
+     {/* styles for image boxes  */}
+    <style>
+    {`
+    .image-container {
+        width: 90%;
+        overflow-x: auto;
+        white-space: nowrap;
+        height: 120px; 
+    }
 
-                .image-box {
-                    width: 190px;
-                    height: 90px;
-                    border: 2px solid white;
-                    margin: 8px;
-                    transition: transform 0.3s ease;
-                }
+    .image-row {
+        display: inline-flex;
+    }
 
-                .image-box:hover {
-                    transform: scale(1.4);
-                }
-                `}
-            </style>
+    .image-box {
+        width: 190px;
+        height: 100px;
+        border: 1px solid grey;
+        margin-right: 10px;
+        transition: transform 0.3s ease;
+    }
+
+    .image-box:last-child {
+        margin-right: 20;
+    }
+
+    .image-box:hover {
+        transform: scale(1.2);
+    }
+    `}
+</style>
         </div>
     );
 }
